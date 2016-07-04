db3012c42b2fe28e0c32a05d99ccdfe3bd4713fa
Quinlan Pfiffer <qpfiffer@gmail.com>
2016-07-04 11:13:32 -0700

    Better pipeline

--- data/create.sh
+++ data/create.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+set -x
 dirs=( courses rides)
 for dir in "${dirs[@]}"; do
     for fit_file in $(ls $dir); do
-        echo $fit_file
+        if [[ $fit_file == *.FIT ]]; then
+            gpsbabel -i garmin_fit -o gpx "./$dir/$fit_file" "./$dir/$(echo $fit_file | sed 's/\.FIT/\.gpx/g')"
+        fi
     done
 done
