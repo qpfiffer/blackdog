06c69a42877fdf19e7d78dcea50fbf316866bfc5
Quinlan Pfiffer <qpfiffer@gmail.com>
2016-07-06 21:34:55 -0700

    Clean up, add courses.

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,22 @@
+function _add_gpx(map, url, color, opacity) {
+    var thing = new L.GPX(url, { async: true,
+        polyline_options: {
+            color: color,
+        opacity: opacity,
+        },
+        marker_options: {
+            //startIconUrl: '/static/img/pin-icon-start.png',
+            //endIconUrl: '/static/img/pin-icon-end.png',
+            //shadowUrl: '/static/img/pin-shadow.png'
+            startIconUrl: null,
+        endIconUrl: null,
+        shadowUrl: null
+        }
+    }).on('loaded', function(e) {
+        map.fitBounds(e.target.getBounds());
+    }).addTo(map);
+}
+
 function create_map() {
     var layer = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {});
 
@@ -14,22 +33,14 @@ function create_map() {
     for (var idx in rides) {
         var ride = rides[idx];
         var gpx = '/data/rides/' + ride + '.gpx';
-        var thing = new L.GPX(gpx, { async: true,
-            polyline_options: {
-                color: '#FA2A00',
-            opacity: 1.0,
-            },
-            marker_options: {
-                //startIconUrl: '/static/img/pin-icon-start.png',
-                //endIconUrl: '/static/img/pin-icon-end.png',
-                //shadowUrl: '/static/img/pin-shadow.png'
-                startIconUrl: null,
-            endIconUrl: null,
-            shadowUrl: null
-            }
-        }).on('loaded', function(e) {
-            map.fitBounds(e.target.getBounds());
-        }).addTo(map);
+        _add_gpx(map, gpx, "#FA2A00", 1.0);
+    }
+
+    var courses = ['The_Oregon_Outback_-_OB_Edition', 'Home_to_Milo'];
+    for (var idx in courses) {
+        var course = courses[idx];
+        var gpx = '/data/courses/' + course + '.gpx';
+        _add_gpx(map, gpx, "#FA2A00", 0.6);
     }
 }
 
