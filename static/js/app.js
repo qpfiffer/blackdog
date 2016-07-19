d5a921eb4e0cfef49302039ff16a1064c01c9767
Quinlan Pfiffer <qpfiffer@gmail.com>
2016-07-18 21:37:58 -0700

    JOURNAL!

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,38 @@
 var map = null;
+
+var campaigns = [
+    {
+        name: "Home to Milo 7/9/16",
+        rides: ['679G1357', '67A82825', '67AA3532'],
+        courses: ['Home_to_Milo'],
+    },
+];
+
+var journalEntries = [
+    {
+        bgImage: "/static/img/journal/bg01.png",
+        title: "Test Entry",
+        entry: "This is a test of the journalling system.",
+        date: "2016/07/18",
+        author: "QP"
+    }
+];
+
+var allData = {
+    message: "TEST!",
+    modalShow: null,
+    showMetaPins: false,
+    financials: [
+        ["Buckles", "1.90"],
+        ["Garmin Edge 20", "60.00"],
+        ["Map of the West Coast", "6.00"]
+    ],
+    campaigns: campaigns,
+    currentCampaign: campaigns[0],
+    journalEntries: journalEntries,
+    currentJournalEntry: journalEntries[0]
+}
+
 function _add_gpx(map, url, color, opacity, showMetaPins) {
     var pinsOptions = {
         startIconUrl: '/static/img/pin-icon-start.png',
@@ -55,28 +89,9 @@ function fill_map(rides, courses, showMetaPins) {
 }
 
 function create_app() {
-    var campaigns = [
-        {
-            name: "Home to Milo 7/9/16",
-            rides: ['679G1357', '67A82825', '67AA3532'],
-            courses: ['Home_to_Milo'],
-        },
-    ];
-
     app = new Vue({
         el: "#main_container",
-        data: {
-            message: "TEST!",
-            modalShow: null,
-            showMetaPins: false,
-            financials: [
-                ["Buckles", "1.90"],
-                ["Garmin Edge 20", "60.00"],
-                ["Map of the West Coast", "6.00"]
-            ],
-            campaigns: campaigns,
-            currentCampaign: campaigns[0]
-        },
+        data: allData,
         methods: {
             changeCampaign: function(idx) {
                 app.currentCampaign = app.campaigns[idx];
