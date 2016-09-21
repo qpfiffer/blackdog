28674bcfa7b25ca0dc9f7cf76946d4caf010358c
Quinlan Pfiffer <qpfiffer@gmail.com>
2016-09-21 09:41:41 -0700

    Update prolems

--- home/forms.py
+++ home/forms.py
@@ -8,4 +8,7 @@ class UploadCampaignForm(forms.Form):
 
     def __init__(self, user, *args, **kwargs):
         super(UploadCampaignForm, self).__init__(*args, **kwargs)
-        self.fields['campaign'].queryset = Campaign.objects.filter(owner=user)
+        if user.is_authenticated:
+            self.fields['campaign'].queryset = Campaign.objects.filter(owner=user)
+        else:
+            self.fields['campaign'].queryset = Campaign.objects.none()
