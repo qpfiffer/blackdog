f9e827bf9d0d19ad6d3e4bbce5b83a5c1a962b8b
Quinlan Pfiffer <qpfiffer@gmail.com>
2016-09-22 14:29:32 -0700

    Fix stupid thing

--- blackdog/settings.py
+++ blackdog/settings.py
@@ -156,8 +156,6 @@ STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
 AWS_ACCESS_KEY_ID=os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY=os.environ['AWS_SECRET_ACCESS_KEY']
-AWS_STORAGE_BUCKET_NAME='blackdog-dev'
-if 'manage' not in sys.argv[0]:
-    AWS_STORAGE_BUCKET_NAME='blackdog-production'
+AWS_STORAGE_BUCKET_NAME=os.environ.get('S3_BUCKET_NAME', 'blackdog-dev')
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
