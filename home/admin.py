35dc354772bb56680188840fa6d25aeea36f0615
Quinlan Pfiffer <qpfiffer@gmail.com>
2016-10-25 19:35:24 -0700

    Add text POIs to admin

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from home.models import Campaign, Ride, Course, PointOfInterest, FinancialCategory, FinancialEntry, Contact, EntryPointOfInterest, InstagramPointOfInterest
+from home.models import Campaign, Ride, Course, PointOfInterest, FinancialCategory, FinancialEntry, Contact, EntryPointOfInterest, InstagramPointOfInterest,\
+                        TextPointOfInterest
 
 admin.site.register(Campaign)
 admin.site.register(Ride)
@@ -8,6 +9,7 @@ admin.site.register(Course)
 admin.site.register(PointOfInterest)
 admin.site.register(EntryPointOfInterest)
 admin.site.register(InstagramPointOfInterest)
+admin.site.register(TextPointOfInterest)
 admin.site.register(FinancialCategory)
 admin.site.register(FinancialEntry)
 admin.site.register(Contact)
